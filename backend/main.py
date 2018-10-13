47b625788676ed7380c06c3713b11736d6629fa1
Sean <sean2108@gmail.com>
2018-10-14 00:22:35 +0800

    change to audio live endpoint

--- backend/main.py
+++ backend/main.py
@@ -3,15 +3,14 @@ from speech_recognizer import convert_to_text
 import json
 
 app = Flask(__name__)
-@app.route('/form')
 
 @app.route('/live', methods=['POST'])
 def process_batch():
-    # data = json.loads(request.data)
-    # audio = base64.b64decode(data.audio)
-    # text, count_of_words = convert_to_text(audio)
-    # return jsonify({"text": text, "count_of_words": count_of_words})
-    return jsonify({"text": "test"})
+    data = json.loads(request.data)
+    audio = base64.b64decode(data.audio)
+    text, count_of_words = convert_to_text(audio)
+    return jsonify({"text": text, "count_of_words": count_of_words})
+    # return jsonify({"text": "test"})
 
 @app.route('/end', methods=['GET'])
 def end():
