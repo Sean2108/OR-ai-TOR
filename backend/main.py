b4c857b588d49f6538c731b9d214bee2401a1c3b
Sean <sean2108@gmail.com>
2018-10-13 23:09:14 +0800

    update backend

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,11 @@ app = Flask(__name__)
 
 @app.route('/live', methods=['POST'])
 def process_batch():
-    data = json.loads(request.data)
-    audio = base64.b64decode(data.audio)
-    text, count_of_words = convert_to_text(audio)
-    return jsonify({"text": text, "count_of_words": count_of_words})
+    # data = json.loads(request.data)
+    # audio = base64.b64decode(data.audio)
+    # text, count_of_words = convert_to_text(audio)
+    # return jsonify({"text": text, "count_of_words": count_of_words})
+    return jsonify({"text": "test"})
 
 @app.route('/end', methods=['GET'])
 def end():
